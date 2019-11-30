d46101779f06c523388bc0c9190f44b8163909bb
Ravenclaw-OIer <yzshangs@126.com>
2019-11-30 12:17:37 +0800

    upload all initial files

---
+++ WangJun/WangJun/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "学号范围"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(126, 8)
+        Me.NumericUpDown2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(50, 23)
+        Me.NumericUpDown2.TabIndex = 11
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {32, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(100, 8)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 15)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "到"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Ubuntu Light", 50.0!)
+        Me.Label5.Location = New System.Drawing.Point(105, 51)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 77)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "0"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Lime
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(82, 140)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(115, 31)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "开始"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(193, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 27)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "初始化"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(100, 189)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 15)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "已抽取"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(161, 189)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 15)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "0"
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button5
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(311, 231)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "学号抽取"
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label4 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ WangJun/WangJun/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Const N As Integer = 200
+    Dim vis As New HashSet(Of Integer)
+    Dim Randomizer As New Random
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim id, res As Integer
+        If (vis.Count() = 0) Then
+            Dim reply = MessageBox.Show("已经全部抽取过一遍。" + vbCrLf + "您想要继续吗？", "学号抽取", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+            If reply = DialogResult.Yes Then
+                init()
+            Else
+                Close()
+            End If
+        End If
+        id = Randomizer.Next(0, vis.Count())
+        res = vis.ElementAt(id)
+        vis.Remove(res)
+        Label5.Text = res
+        Label2.Text = (Val(Label2.Text) + 1).ToString
+    End Sub
+    Private Sub init()
+        For i As Integer = 1 To NumericUpDown2.Value
+            vis.Add(i)
+        Next
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        init()
+        NumericUpDown2.Enabled = False
+        Button5.Enabled = True
+        Button1.Enabled = False
+    End Sub
+End Class
