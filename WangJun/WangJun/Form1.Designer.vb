b65e44f4d68906da5e4c294833ea166a4f06015c
Ravenclaw-OIer <yzshangs@126.com>
2020-01-04 14:45:27 +0800

    Added save feature

--- WangJun/WangJun/Form1.Designer.vb
+++ WangJun/WangJun/Form1.Designer.vb
@@ -23,45 +23,20 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.文件ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.新建ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.打开OToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.另存为AToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.关于AToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(38, 8)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(59, 15)
-        Me.Label3.TabIndex = 9
-        Me.Label3.Text = "学号范围"
-        '
-        'NumericUpDown2
-        '
-        Me.NumericUpDown2.Location = New System.Drawing.Point(126, 8)
-        Me.NumericUpDown2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
-        Me.NumericUpDown2.Name = "NumericUpDown2"
-        Me.NumericUpDown2.Size = New System.Drawing.Size(50, 23)
-        Me.NumericUpDown2.TabIndex = 11
-        Me.NumericUpDown2.Value = New Decimal(New Integer() {32, 0, 0, 0})
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(100, 8)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(20, 15)
-        Me.Label4.TabIndex = 12
-        Me.Label4.Text = "到"
         '
         'Label5
         '
@@ -76,21 +51,13 @@ Partial Class Form1
         'Button5
         '
         Me.Button5.BackColor = System.Drawing.SystemColors.Control
+        Me.Button5.Enabled = False
         Me.Button5.Location = New System.Drawing.Point(82, 140)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(115, 31)
         Me.Button5.TabIndex = 14
         Me.Button5.Text = "开始"
         Me.Button5.UseVisualStyleBackColor = False
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(193, 5)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(84, 27)
-        Me.Button1.TabIndex = 15
-        Me.Button1.Text = "初始化"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -110,14 +77,49 @@ Partial Class Form1
         Me.Label2.TabIndex = 17
         Me.Label2.Text = "0"
         '
-        'Button2
+        'MenuStrip1
         '
-        Me.Button2.Location = New System.Drawing.Point(224, 185)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 18
-        Me.Button2.Text = "关于"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.文件ToolStripMenuItem, Me.关于AToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(311, 24)
+        Me.MenuStrip1.TabIndex = 19
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '文件ToolStripMenuItem
+        '
+        Me.文件ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.新建ToolStripMenuItem, Me.打开OToolStripMenuItem, Me.另存为AToolStripMenuItem})
+        Me.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem"
+        Me.文件ToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.文件ToolStripMenuItem.Text = "文件(&F)"
+        '
+        '新建ToolStripMenuItem
+        '
+        Me.新建ToolStripMenuItem.Name = "新建ToolStripMenuItem"
+        Me.新建ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.新建ToolStripMenuItem.Text = "新建(&N)"
+        '
+        '打开OToolStripMenuItem
+        '
+        Me.打开OToolStripMenuItem.Name = "打开OToolStripMenuItem"
+        Me.打开OToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.打开OToolStripMenuItem.Text = "打开(&O)"
+        '
+        '另存为AToolStripMenuItem
+        '
+        Me.另存为AToolStripMenuItem.Name = "另存为AToolStripMenuItem"
+        Me.另存为AToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.另存为AToolStripMenuItem.Text = "保存(&S)"
+        '
+        '关于AToolStripMenuItem
+        '
+        Me.关于AToolStripMenuItem.Name = "关于AToolStripMenuItem"
+        Me.关于AToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.关于AToolStripMenuItem.Text = "关于(&A)"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
         'Form1
         '
@@ -125,32 +127,33 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(311, 231)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.NumericUpDown2)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Form1"
         Me.Text = "学号抽取"
-        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label4 As Label
-    Friend WithEvents NumericUpDown2 As NumericUpDown
-    Friend WithEvents Label3 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Button5 As Button
-    Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 文件ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 新建ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 打开OToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 另存为AToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 关于AToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
 End Class
